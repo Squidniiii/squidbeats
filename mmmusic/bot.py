9f9ec1530f282c7a7daaaeb2cd40867d4b0332f3
Sydney Wang <sgwang2@illinois.edu>
2021-12-20 14:40:28 -0600

    POV you forgot to checkout a new branch

--- mmmusic/bot.py
+++ mmmusic/bot.py
@@ -1,13 +1,18 @@
 # bot.py
-import os
-
 import discord
+from discord.ext import commands,tasks
+import os
 from dotenv import load_dotenv
+import youtube_dl
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+# client = discord.Client()
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='s!',intents=intents)
 
 @client.event
 async def on_ready():
